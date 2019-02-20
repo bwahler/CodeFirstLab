8eecdc5f62664bb66105950029973f502c714611
bwahler <bmwahler@gmail.com>
2019-02-19 22:27:29 -0500

    Initial Commit

---
+++ ComfyClothes/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ComfyClothes.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class ComfyDb : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
